cfe64145082d80fe6a73d0cab67b4e5ad4b2b28f
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2025-06-17 09:32:05 +0200

    c

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,5 +12,10 @@ class DemoApplicationTests {
 	void contextLoads() {
 		assertTrue(true, "alles gut");
 	}
+
+	@Test
+    void testSomething() {
+        assertEquals(4, 2 + 2);
+    }
 
 }
